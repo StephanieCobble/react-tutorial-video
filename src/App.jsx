ddbc1a7c9b95a21357f26bb1940d51df40d58f4a
StephanieCobble <cobble.stephanie@gmail.com>
2022-04-04 12:26:43 -0700

    added entry folders and files and wireframe example

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+   
+import React, { useState } from 'react';
+import DisplayEntries from './Components/DisplayEntries/DisplayEntries'
+import AddEntryForm from './Components/AddEntry/AddEntryForm';
+import EntriesChartTracker from './Components/EntriesChartTracker/EntriesChartTracker';
+
+
+function App() {
+  
+  const [entries, setEntries] = useState([{weight: 175, date: '11-23-2021'}, {weight: 180, date: '11-24-2021'}])
+
+  function addNewEntry (entry) {
+    let tempEntries = [...entries, entry];
+    setEntries(tempEntries);
+  }
+  return (
+    <div>
+      <DisplayEntries parentEntries = {entries}/>
+      <AddEntryForm addNewEntryProperty={addNewEntry}/>
+      <EntriesChartTracker parentEntries = {entries}/>
+    </div>
+  );
+}
+export default App;
